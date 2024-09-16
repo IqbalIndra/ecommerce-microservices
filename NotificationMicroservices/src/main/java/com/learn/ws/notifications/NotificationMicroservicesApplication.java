701031b2d1ca4c1e443aba6cdfc1caaac6f16598
iqbal.indra <iqbal.indra@karmaclub.id>
2024-09-16 15:00:02 +0700

    Initial Project for Notification service

---
+++ NotificationMicroservices/src/main/java/com/learn/ws/notifications/NotificationMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.ws.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationMicroservicesApplication.class, args);
+	}
+
+}
