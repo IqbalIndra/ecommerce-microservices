868b33b0e33aa49aff7f11455d1b4d57b62d80e4
iqbal.indra <iqbal.indra@karmaclub.id>
2024-09-16 23:43:59 +0700

    Add Producer factory with bean configuration , Add max flight per request in .properties

--- ProductMicroservices/src/main/java/com/learn/ws/products/KafkaConfig.java
+++ ProductMicroservices/src/main/java/com/learn/ws/products/KafkaConfig.java
@@ -1,14 +1,28 @@
 package com.learn.ws.products;
 
+import com.learn.ws.core.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    private final Environment env;
+
+    public KafkaConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public NewTopic createTopic(){
@@ -17,5 +31,26 @@ public class KafkaConfig {
                 .replicas(3)
                 .configs(Map.of("min.insync.replicas","2"))
                 .build();
+    }
+
+    @Bean
+    public ProducerFactory<String, ProductCreatedEvent> producerFactory(){
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.producer.bootstrap-servers"));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.ACKS_CONFIG, env.getProperty("spring.kafka.producer.acks"));
+        config.put(ProducerConfig.LINGER_MS_CONFIG, env.getProperty("spring.kafka.producer.properties.linger.ms"));
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, env.getProperty("spring.kafka.producer.properties.request.timeout.ms"));
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, env.getProperty("spring.kafka.producer.properties.max.in.flight.requests.per.connection"));
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, env.getProperty("spring.kafka.producer.properties.enable.idempotence"));
+
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String,ProductCreatedEvent> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 }
