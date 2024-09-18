7d52e405d798410a7e1fbdecb494209222638396
iqbal.indra <iqbal.indra@karmaclub.id>
2024-09-18 12:35:36 +0700

    update env application properties for group-id

--- NotificationMicroservices/src/main/java/com/learn/ws/notifications/KafkaConsumerConfiguration.java
+++ NotificationMicroservices/src/main/java/com/learn/ws/notifications/KafkaConsumerConfiguration.java
@@ -35,7 +35,7 @@ public class KafkaConsumerConfiguration {
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("consumer.group-id"));
         config.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
 
         return new DefaultKafkaConsumerFactory<>(config);
